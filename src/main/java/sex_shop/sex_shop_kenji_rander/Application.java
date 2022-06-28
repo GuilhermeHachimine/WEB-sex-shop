ac5d9553fb82ac7896a06a0f611f75a15e040546
Guilherme Hachimine <gui.hachimine@gmail.com>
2022-06-28 20:59:38 -0300

    initial commit

---
+++ src/main/java/sex_shop/sex_shop_kenji_rander/Application.java
@@ -0,0 +1,19 @@
+package sex_shop.sex_shop_kenji_rander;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String[] args) {
+		logger.info(">>>> Iniciando a execucao da aplicacao");
+		SpringApplication.run(Application.class, args);
+		logger.info(">>>> Aplicacao inicializada");
+	}
+
+}
