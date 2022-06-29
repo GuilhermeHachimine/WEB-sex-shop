6ffc4db6af8d40b0b7e943dc3d3adb4cbf2877ee
Guilherme Hachimine <gui.hachimine@gmail.com>
2022-06-29 18:00:29 -0300

    feat Produto Repository

---
+++ src/main/java/sex_shop/sex_shop_kenji_rander/model/Produto.java
@@ -0,0 +1,78 @@
+package sex_shop.sex_shop_kenji_rander.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "produto")
+public class Produto implements Serializable{
+	private static final long serialVersionUID = 1526262475932778296L;// gere um outro valor
+	@Id
+	@SequenceGenerator(name="gerador", sequenceName="produto_codigo_seq", allocationSize=1)
+	@GeneratedValue(generator="gerador", strategy=GenerationType.SEQUENCE)
+	private Long codigo;
+	private String nome;
+	private Long preco;
+	private String img_url;
+	
+	public Long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Long getPreco() {
+		return preco;
+	}
+	public void setPreco(Long preco) {
+		this.preco = preco;
+	}
+	public String getImg_url() {
+		return img_url;
+	}
+	public void setImg_url(String img_url) {
+		this.img_url = img_url;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return "Produto [codigo=" + codigo + ", nome=" + nome + ", preco=" + preco + ", img_url=" + img_url + "]";
+	}
+}
---
+++ src/main/java/sex_shop/sex_shop_kenji_rander/repository/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package sex_shop.sex_shop_kenji_rander.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import sex_shop.sex_shop_kenji_rander.model.Produto;
+
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+
+}
