ee395aa09eb61366457d829f51378e6ae51b58ac
Guilherme Hachimine <gui.hachimine@gmail.com>
2022-06-28 21:52:58 -0300

    basic security

---
+++ src/main/java/sex_shop/sex_shop_kenji_rander/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package sex_shop.sex_shop_kenji_rander.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				// Qualquer um pode fazer requisições para essas URLs
+				.antMatchers("/css/**", "/js/**" ).permitAll()
+				// Um usuário autenticado e com o papel ADMIN pode fazer requisições para essas URLs	
+				.antMatchers("/", "/index.html").hasRole("ADMIN")
+				//.antMatchers("URL").hasAnyRole("ADMIN", "USUARIO")
+				.and()
+			// A autenticação usando formulário está habilitada 
+			.formLogin();
+	}
+
+}
