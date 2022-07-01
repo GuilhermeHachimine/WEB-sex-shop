5ca62a52bae8188a94259c403c25f5d3d6d9f7b0
Guilherme Hachimine <gui.hachimine@gmail.com>
2022-07-01 10:45:06 -0300

    feat: redirect to buy

---
+++ src/main/java/sex_shop/sex_shop_kenji_rander/controller/CompraController.java
@@ -0,0 +1,26 @@
+package sex_shop.sex_shop_kenji_rander.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import sex_shop.sex_shop_kenji_rander.model.Produto;
+
+@Controller
+@RequestMapping("/comprar")
+public class CompraController {
+	private static final Logger logger = LoggerFactory.getLogger(UsuarioController.class);
+	
+	
+	@PostMapping("/produtos")
+	public String abrirEscolhaVacina(Produto produto, Model model) {
+		logger.trace("Entrou em direcionar compra produto");
+		logger.trace("Produto a ser comprado {}",produto );
+		model.addAttribute("produto", produto);
+		logger.trace("direcionando para compraproduto");
+		return "/compraproduto";
+	}
+}
